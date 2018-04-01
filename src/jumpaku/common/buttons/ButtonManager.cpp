59f3f85c48be2765f679b8ae689007abf3bc19ef
Jumpaku <itzimeru@gmail.com>
2018-04-02 04:18:33 +0900

    added ButtonManager

---
+++ src/jumpaku/common/buttons/ButtonManager.cpp
@@ -0,0 +1,8 @@
+//
+//  ButtonManager.cpp
+//  KaleidoscopicSeal
+//
+//  Created by Jumpaku on 2018/04/02.
+//
+
+#include "ButtonManager.hpp"
---
+++ src/jumpaku/common/buttons/ButtonManager.hpp
@@ -0,0 +1,13 @@
+//
+//  ButtonManager.hpp
+//  KaleidoscopicSeal
+//
+//  Created by Jumpaku on 2018/04/02.
+//
+
+#ifndef ButtonManager_hpp
+#define ButtonManager_hpp
+
+#include <stdio.h>
+
+#endif /* ButtonManager_hpp */
