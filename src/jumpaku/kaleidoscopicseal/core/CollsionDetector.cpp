5327f9ab204d9b34279ed5dcbe16d3001ddb4f57
Jumpaku <itzimeru@gmail.com>
2018-04-02 04:43:07 +0900

    implemented TestHit

---
+++ src/jumpaku/kaleidoscopicseal/core/CollsionDetector.cpp
@@ -0,0 +1,8 @@
+//
+//  CollsionDetector.cpp
+//  KaleidoscopicSeal
+//
+//  Created by Jumpaku on 2018/04/02.
+//
+
+#include "CollsionDetector.hpp"
---
+++ src/jumpaku/kaleidoscopicseal/core/CollsionDetector.hpp
@@ -0,0 +1,13 @@
+//
+//  CollsionDetector.hpp
+//  KaleidoscopicSeal
+//
+//  Created by Jumpaku on 2018/04/02.
+//
+
+#ifndef CollsionDetector_hpp
+#define CollsionDetector_hpp
+
+#include <stdio.h>
+
+#endif /* CollsionDetector_hpp */
